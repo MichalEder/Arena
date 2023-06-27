09dfe9a55dc50cbecbe8d6508e2de3c94f4d2cc3
MichalEder <103189873+MichalEder@users.noreply.github.com>
2023-06-27 08:49:40 +0200

    tvorba kostky

---
+++ arena.py
@@ -0,0 +1,17 @@
+class Kostka:
+    """
+    Třída reprezentuje hrací kostku
+
+        """
+def __init__(self, pocet sten):
+    self.__pocet_sten = pocet_sten
+
+def vrat_pocet_sten(self):
+    """
+    Vrátí počet stěn
+    """
+    return self.__pocet_sten
+
+
+kostka = Kostka()
+print(kostka.pocet_sten)
